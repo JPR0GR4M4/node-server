ba45595bcb86429196be9c6c2968b2f2b9580766
Juan Pablo Caro <125841812+JPR0GR4M4@users.noreply.github.com>
2023-07-31 19:17:23 -0500

    Update list-view-router.js

--- list-view-router.js
+++ list-view-router.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const validateMiddleware = (req, res, next) => {
+  const { id } = req.params;
+  if (!/^\d+$/.test(id)) {
+    return res.status(400).json({ error: "Invalid parameter." });
+  }
+  next();
+};
+
 module.exports = (tasks) => {
   router.get("/completed", (req, res) => {
     const completedTasks = tasks.filter((task) => task.completed);
@@ -10,6 +18,17 @@ module.exports = (tasks) => {
   router.get("/incomplete", (req, res) => {
     const incompleteTasks = tasks.filter((task) => !task.completed);
     res.json(incompleteTasks);
+  });
+
+  router.get("/:id", validateMiddleware, (req, res) => {
+    const { id } = req.params;
+    const task = tasks.find((task) => task.id === parseInt(id));
+
+    if (task) {
+      res.json(task);
+    } else {
+      res.status(404).json({ error: "Task not found for the given ID." });
+    }
   });
 
   return router;
