6f470eac4f88297ae099dfdb7a0023b6786d363b
Juan Pablo Caro <125841812+JPR0GR4M4@users.noreply.github.com>
2023-07-31 22:04:08 -0500

    Update list-view-router.js

--- list-view-router.js
+++ list-view-router.js
@@ -14,22 +14,18 @@ module.exports = (tasks) => {
     const completedTasks = tasks.filter((task) => task.completed);
     res.json(completedTasks);
   });
-
   router.get("/incomplete", (req, res) => {
     const incompleteTasks = tasks.filter((task) => !task.completed);
     res.json(incompleteTasks);
   });
-
   router.get("/:id", validateMiddleware, (req, res) => {
     const { id } = req.params;
     const task = tasks.find((task) => task.id === parseInt(id));
-
     if (task) {
       res.json(task);
     } else {
       res.status(404).json({ error: "Task not found for the given ID." });
     }
   });
-
   return router;
 };
