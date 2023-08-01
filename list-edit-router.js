4ce51868bad7fff4ae80c287eedf942683140592
Juan Pablo Caro <125841812+JPR0GR4M4@users.noreply.github.com>
2023-07-31 19:14:21 -0500

    Update list-edit-router.js

--- list-edit-router.js
+++ list-edit-router.js
@@ -2,7 +2,32 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (tasks, taskId) => {
-  router.post("/create", (req, res) => {
+  const validateRequestBody = (req, res, next) => {
+    if (req.method === "POST" && Object.keys(req.body).length === 0) {
+      res.status(400).json({ error: "The request has no data." });
+    } else if (
+      req.method === "POST" &&
+      (!req.body.title || typeof req.body.title !== "string")
+    ) {
+      res
+        .status(400)
+        .json({ error: "The request has an invalid description." });
+    } else if (req.method === "PUT" && Object.keys(req.body).length === 0) {
+      res.status(400).json({ error: "The request has no data." });
+    } else if (
+      req.method === "PUT" &&
+      (req.body.completed === undefined ||
+        typeof req.body.completed !== "boolean")
+    ) {
+      res.status(400).json({
+        error: "The request has an invalid completed status.",
+      });
+    } else {
+      next();
+    }
+  };
+  
+  router.post("/create", validateRequestBody, (req, res) => {
     const { title } = req.body;
     const id = taskId++;
 
@@ -25,19 +50,19 @@ module.exports = (tasks, taskId) => {
       tasks.splice(taskIndex, 1);
       res.json({ message: "The task has been successfully deleted." });
     } else {
-      res.status(404).json({ error: "Task not found." });
+      res.status(404).json({ error: "Task not found for the given ID." });
     }
   });
 
-  router.put("/update/:id", (req, res) => {
+  router.put("/update/:id", validateRequestBody, (req, res) => {
     const id = parseInt(req.params.id);
 
     const task = tasks.find((task) => task.id === id);
     if (task) {
-      task.completed = true;
+      task.completed = req.body.completed;
       res.json({ message: "The task has been successfully updated.", task });
     } else {
-      res.status(404).json({ error: "Task not found." });
+      res.status(404).json({ error: "Task not found for the given ID." });
     }
   });
 
