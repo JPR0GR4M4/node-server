d242018fcf2b66e24943e86c29b407b83dad1258
Juan Pablo Caro <125841812+JPR0GR4M4@users.noreply.github.com>
2023-07-31 21:54:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,16 +3,10 @@ const app = express();
 
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
-
 dotenv.config();
 
 const listViewRouter = require("./list-view-router.js");
 const listEditRouter = require("./list-edit-router.js");
-
-const users = [
-  { username: "George", password: "8642159" },
-  { username: "Thomas", password: "3094782" },
-];
 
 app.use(express.json());
 
@@ -28,35 +22,49 @@ app.use(validateMiddleware);
 
 let tasks = [];
 let taskId = 1;
+const registeredUsers = [];
+
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ error: "You must enter a username and password." });
+  }
+  const existingUser = registeredUsers.find(
+    (user) => user.username === username
+  );
+  if (existingUser) {
+    return res.status(400).json({
+      error: "The username already exists, choose another one.",
+    });
+  }
+  const newUser = { username, password };
+  registeredUsers.push(newUser);
+  res.json({ message: "User created successfully." });
+});
 
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
-
-  const user = users.find(
+  const user = registeredUsers.find(
     (u) => u.username === username && u.password === password
   );
-
   if (!user) {
     return res.status(401).json({ error: "Incorrect data." });
   }
-
   const token = jwt.sign({ username }, process.env.JWT_SECRET);
-
   res.json({ token });
 });
 
 app.get("/protected-route", (req, res) => {
   const token = req.headers.authorization;
-
   if (!token) {
     return res.status(401).json({ error: "Token not given." });
   }
-
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
       return res.status(401).json({ error: "Invalid Token." });
     }
-
     res.json({ message: "Successful access to the protected route." });
   });
 });
